49e95ec2d25160cc76a93a9a131a7f350bd9d63f
xiao <jake_xiao@yeah.net>
2019-04-11 11:30:43 +0800

    2019.3.31

--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-from typing import List
-
-
-class Solution:
-    def repeatedNTimes(self, A: List[int]) -> int:
-        lenth = len(A)/2
-        unique = set(A)
-
-        for x in unique:
-            count = 0
-            for y in A:
-                if x == y:
-                    count += 1
-            if count == lenth:
-                return x
+if __name__ == '__main__':
+    test = dict()
+    print(test.get('r'))
